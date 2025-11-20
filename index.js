ae9c544911fde19e4655cc6ef3d816227cbdd7d1
ALNATU2025 <alusinebarrie578@gmail.com>
2025-11-20 11:00:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const path = require('path');
 const fs = require('fs');
 const { body, validationResult, query } = require('express-validator');
 const NodeCache = require('node-cache');
+const User = require('./models/User');
 
 // Try to load security middleware with error handling
 let helmet, rateLimit, mongoSanitize, xss, hpp, moment;
