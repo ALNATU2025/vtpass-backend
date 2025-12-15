4e2ee413665f3fa2be116ce32205bdd537aff7f7
ALNATU2025 <alusinebarrie578@gmail.com>
2025-12-15 17:04:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -210,14 +210,6 @@ const protect = async (req, res, next) => {
   }
 };
 
-// Token generation helpers
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-};
-
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
-};
 
 
 
